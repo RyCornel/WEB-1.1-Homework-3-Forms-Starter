02002866f1db249ec89838f4b08af71aa6d500e6
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-05-03 02:19:47 -0700

    Commit 3
    Updated compliments_results() function & compliments_results.html

--- app.py
+++ app.py
@@ -55,12 +55,16 @@ def compliments():
 @app.route('/compliments_results')
 def compliments_results():
     """Show the user some compliments."""
+    for compliment in list_of_compliments:
+        random.sample(list_of_compliments, k = int(request.args.get('num_compliments')))
+        return compliment
 
     context = {
         # TODO: Enter your context variables here.
-        'users_name' : request.args.get('users_name'),
-        'wants_compliments' : request.args.get('wants_compliments'),
-        'num_compliments' : int(request.args.get('num_compliments'))
+        'name' : request.args.get('users_name'),
+        'want_compliments' : request.args.get('wants_compliments'),
+        'compliments' : compliment_list
+
     }
 
     return render_template('compliments_results.html', **context)
